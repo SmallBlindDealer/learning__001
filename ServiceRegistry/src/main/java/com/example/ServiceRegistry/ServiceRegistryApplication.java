5ae38e0b4116e50db60b860e8711e7f29430e83a
shivshankarkeshari <shiv.s.keshari@gmail.com>
2024-12-19 10:41:31 +0530

    client server setup info

--- ServiceRegistry/src/main/java/com/example/ServiceRegistry/ServiceRegistryApplication.java
+++ ServiceRegistry/src/main/java/com/example/ServiceRegistry/ServiceRegistryApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer // service-discovery server
+@EnableEurekaServer // service-discovery server 
+// @EnableEurekaClient <--- for client server
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
