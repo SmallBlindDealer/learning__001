72364dd3e1a26c081369fdfec72d7be2257a4e0e
shivshankarkeshari <shiv.s.keshari@gmail.com>
2025-01-13 14:02:49 +0530

    flink

---
+++ flink_impl/readme.md
@@ -0,0 +1,9 @@
+
+
+pip install confluent-kafka
+pip install apache-flink
+
+bin/kafka-console-consumer.sh --topic sales-usd --from-beginning --bootstrap-server localhost:9092
+
+
+
--- server.py
+++ server.py
@@ -8,3 +8,43 @@ def hello_world():
 
 
 # flask --app server run -h 192.168.1.3
+
+
+
+"""
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# A route for a GET request
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Sample data that will be returned as JSON
+    data = {
+        'name': 'John Doe',
+        'age': 30,
+        'city': 'New York'
+    }
+    return jsonify(data)
+
+# Another example with query parameters
+@app.route('/api/user', methods=['GET'])
+def get_user():
+    # Retrieve query parameters
+    name = request.args.get('name', default='Guest')
+    age = request.args.get('age', default=0, type=int)
+
+    # Construct response
+    user = {
+        'name': name,
+        'age': age
+    }
+    return jsonify(user)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
+
+"""
